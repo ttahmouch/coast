6a5b5728692183da04aa08487eeea25b46fa5496
Tony Tahmouch <ttahmouch@gmail.com>
2013-03-03 17:44:47 -0500

    Removed unnecssary comments, and added new ones. Removed unnecessary call to app.configure() that was used for all environments.

--- server.js
+++ server.js
@@ -3,34 +3,39 @@ var express = require('express'),
     path = require('path'),
     app = module.exports = exports = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+/**
+ * Set properties on the app object.
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-  /**
-   * middleware
-   */
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'static')));
-});
+/**
+ * Set middleware to use before and after routing.
+ */
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+/**
+ * Set middleware that's only applicable when running the application in development mode.
+ */
+app.configure('development', function () {
+    app.use(express.errorHandler());
 });
 
 /**
- * routes
- * app.get
- * app.post
- * app.put
- * app.del
+ * Set up the routes to be iterated in the router.
  */
 app.get('/', require('./controllers/controllerIndex.js'));
+app.get();
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+/**
+ * Create an instance of a Node Server, and pass in the Express App as a RequestListener.
+ * Have the server start listening on the port specified as an Environment Variable (or 3000).
+ */
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
