0415ab9465221988530be0018337b88287be5246
Tony Tahmouch <ttahmouch@gmail.com>
2013-03-03 23:51:57 -0500

    Added in all the new middleware and removed various comments. Organized a few other things. 030313.

--- server.js
+++ server.js
@@ -3,39 +3,24 @@ var express = require('express'),
     path = require('path'),
     app = module.exports = exports = express();
 
-/**
- * Set properties on the app object.
- */
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-/**
- * Set middleware to use before and after routing.
- */
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(require('./middleware/middlewareResponseMethods.js'));
+app.use(require('./middleware/middlewareResponseRemovePoweredBy.js'));
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(require('./middleware/middlewareErrorLog'));
+app.use(require('./middleware/middlewareError'));
 
-/**
- * Set middleware that's only applicable when running the application in development mode.
- */
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
+app.all('/', require('./controllers/controllerIndex.js'));
+app.all('*', require('./controllers/controller404.js'));
 
-/**
- * Set up the routes to be iterated in the router.
- */
-app.get('/', require('./controllers/controllerIndex.js'));
-app.get();
-
-/**
- * Create an instance of a Node Server, and pass in the Express App as a RequestListener.
- * Have the server start listening on the port specified as an Environment Variable (or 3000).
- */
 http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server started in ' + app.get('env').toString() + ' mode.');
     console.log("Express server listening on port " + app.get('port'));
 });
