b3853e4e880a34d27a721ae6b36be371007beb4b
Tony Tahmouch <ttahmouch@gmail.com>
2014-08-18 20:56:55 -0400

    #deleted:    api/api.json #deleted:    api/directory/structure/doesnt/matter/entry.js #deleted:    api/directory/structure/payment.js #deleted:    api/useragent.js
    Removed the default api.
    There is no need for the default api within the context of the project.
    Will make accompanying projects with different types of APIs.

--- api/directory/structure/doesnt/matter/entry.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (env, next) {
-    env.response.statusCode = 200;
-    if (env.request.headers['accept'] === 'multipart/nav-data') {
-        env.hypermedia = ['entry', 'payment'];
-    }
-    next(env);
-};
