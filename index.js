08bd48d0d7740679aac9e0d23a4bcbf979037665
Tony Tahmouch <ttahmouch@gmail.com>
2014-08-19 01:07:59 -0400

    modified:   index.js
    Modified the index.js entry point script heavily.
    There is a description of usage from the command line in the header comment section.
    The project now has an immediate dependency on Argo and Argo middleware. They used to be in the hypermedia library dependency.
    The project now parses API JSON DSL files instead of attempting to evaluate Javascript files.
    The CLI now only accepts 2 arguments, --debug and --dir=/path/to/api/directory. This heavily simplified the interface.
    All previous argument semantics have been moved into fields within the JSON file.
    The directory specified is walked synchronously until a file of the name api.json is found for API configuration.
    The directory is also search for files that correspond to middleware and route IDs specified in the JSON file.
    An HTTP server will always be instantiated and run after the script executes, whether or not appropriate files were found in the search.
    If no files are found, the HTTP server will have no middleware or route handlers. Otherwise, respective middleware and route handlers will be mapped.
    Argo orders them as request middleware, request route, response route, response middleware. Request middleware in defined order. Response middleware reversed.

--- index.js
+++ index.js
@@ -2,126 +2,261 @@
 
 /**
  * Falsy Values: false, 0, "", null, undefined, NaN
+ *
+ * - Command Line Usage:
+ * node index.js --debug --dir=/path/to/api.json/and/handler/subdirectories
  */
-
 (function () {
-    var fs = require('fs'),
+    /**
+     * Module Dependencies.
+     * @type {exports}
+     */
+    var http = require('http'),
         path = require('path'),
+        argo = require('argo'),
+        router = require('argo-url-router'),
         hypermedia = require('hypermedia'),
-        slop = require('slop'),
-        globalObjects = 'global,process,console,setTimeout,clearTimeout,setInterval,clearInterval,Buffer',
-        moduleObjects = 'require,__filename,__dirname,module,exports',
-        body = 'var ' + globalObjects + ',' + moduleObjects + ';\r\nreturn ',
-        hypermediator = hypermedia.hypermediator,
-        plugin = hypermedia.plugin,
-        argo = hypermedia.argo;
+        slop = require('slop');
 
     /**
-     * Process.argv will be an array.
-     * Array:
-     *  1st element will be 'node'
-     *  2nd element will be '/path/to/this/JavaScript/file'
-     *  3rd - Nth elements will be additional command line arguments
+     * Walks a file system directory, and invokes the callback function at every encountered file.
      *
-     * Introspect Node arguments vector for:
-     * - path to the hapi dsl file
-     * - optional port to begin accepting connections
-     * - optional host to begin accepting connections
-     * - optional HTTP router library
+     * @param dir {string} representing a file system directory.
+     * @example "/"
      *
-     * Options.get() values may yield:
-     * - {undefined}
-     * - {boolean} true
-     * - {string} (non-empty)
+     * @param callback {Function} with a single 'file' path argument.
+     * @example
+     * function (file) {
+     *     var path = require('path'),
+     *         name = path.basename(file);
+     * }
      *
-     * Options.has() values may yield:
-     * - {boolean} true
-     * - {boolean} false
+     * @return {undefined}
      */
-    var options = slop().parse(process.argv),
-        hapi = options.get('hapi'),
-        port = options.get('port'),
-        host = options.get('host'),
-        lib = options.get('lib'),
-        debug = options.has('debug');
+    function walkSync(dir, callback) {
+        if (typeof dir === 'string' && typeof callback === 'function') {
+            try {
+                var fs = require('fs'),
+                    path = require('path'),
+                    stat = fs.lstatSync(dir);
+                if (stat.isFile()) {
+                    callback(dir);
+                } else if (stat.isDirectory()) {
+                    try {
+                        fs.readdirSync(dir).forEach(function (sub) {
+                            walkSync(path.join(dir, sub), callback);
+                        });
+                    } catch (error) {
+                        console.error('The path, ' + dir + ', cannot be walked.');
+                    }
+                }
+            } catch (error) {
+                console.error('The path, ' + dir + ', does not exist.');
+            }
+        }
+    }
+
+    /**
+     * Walks a file system directory, and looks for Node Javascript files that export a handler function.
+     *
+     * @param dir {string} representing a file system directory.
+     * @example "/"
+     *
+     * @param id {string} representing the name of the file without the ".js" extension.
+     * @example "name"
+     *
+     * @return {Function} if a handler function was exported; {null}, otherwise.
+     */
+    function handlerById(dir, id) {
+        var handler = null;
+        if (typeof id === 'string' && typeof dir === 'string') {
+            walkSync(dir, function (file) {
+                var path = require('path'),
+                    name = path.basename(file, '.js');
+                if (name === id) {
+                    if (debug) {
+                        console.log('The ' + id + '.js directory is ' + file + '.');
+                    }
+                    try {
+                        handler = require(file);
+                    } catch (error) {
+                        handler = null;
+                    }
+                }
+            });
+        }
+        return typeof handler === 'function' ? handler : null;
+    }
+
+    /**
+     * Command Line Options.
+     * Dir will resolve a given path, or be the current working directory.
+     */
+    var opt = slop().parse(process.argv),
+        dir = path.resolve(typeof opt.get('dir') === 'string' ? opt.get('dir') : ''),
+        debug = opt.has('debug');
 
     if (debug) {
-        console.log();
-        console.log('-------------------------');
-        console.log('Before Hosting Attempt:');
-        console.log('Pwd:\r\n' + JSON.stringify(process.cwd()));
-        console.log('Hapi:\r\n' + JSON.stringify(hapi));
-        console.log('Port:\r\n' + JSON.stringify(port));
-        console.log('Host:\r\n' + JSON.stringify(host));
-        console.log('Lib:\r\n' + JSON.stringify(lib));
-        console.log('-------------------------');
-        console.log('During Hosting Attempt:');
+        console.log('The cwd directory is ' + process.cwd() + '.');
+        console.log('The api directory is ' + dir + '.');
     }
 
-    if (typeof hapi === 'string') {
-        /**
-         * Resolves a relative path to an absolute path.
-         *
-         * Path.resolve() documentation:
-         * PWD (Directory of Node binary execution) is used to resolve.
-         * Resulting paths are normalized.
-         * Trailing slashes are removed unless the path is '/'.
-         */
-        hapi = path.resolve(hapi);
+    /**
+     * HTTP Application Router.
+     */
+    var app = argo(),
+        hapi = {};
 
-        if (debug) {
-            console.log('Hapi:\r\n' + JSON.stringify(hapi));
-        }
-
-        /**
-         * Results:
-         * {string} (empty)
-         * {string} (non-empty)
-         */
-        hapi = fs.existsSync(hapi) && fs.statSync(hapi).isFile() ? fs.readFileSync(hapi, 'utf8') : '';
-
-        if (debug) {
-            console.log('Input Hapi:\r\n' + JSON.stringify(hapi));
-        }
-
-        /**
-         * If the string is non-empty,
-         * Then attempt to execute it as literal JavaScript dynamically at runtime.
-         */
-        if (!!hapi) {
-            /**
-             * Hide all global and module scoped Objects in the execution context to mitigate security issues.
-             *
-             * Dynamically created Function Objects:
-             * - do not create closures to their creation contexts
-             * - are created in the global scope
-             * - can access their local variables and globals
-             * - are marginally safer than eval
-             *
-             * TODO: Change to higher-level, external Domain Specific Language that can be tokenized.
-             */
+    /**
+     * Walk the given directory in an attempt to find and parse a file called api.json.
+     */
+    walkSync(dir, function (file) {
+        var name = path.basename(file, '.json');
+        if (name === 'api') {
+            if (debug) {
+                console.log('The api.json directory is ' + file + '.');
+            }
             try {
-                hapi = new Function('hapi', body + hapi)(hypermedia.hapi);
-            } catch (e) {
-                hapi = null;
-                console.log(e);
-            }
-
-            /**
-             * Only mediate if an instance of HypermediaApi was created above.
-             * Library defaults to Argo.
-             * Supporting routers like ExpressJS will need new Plugins.
-             */
-            if (hypermedia.hapi.isPrototypeOf(hapi)) {
-                hypermediator(plugin.isPrototypeOf(hypermedia[lib]) ? hypermedia[lib] : argo)
-                    .setPort(port)
-                    .setHost(host)
-                    .mediate(hapi);
+                hapi = require(file);
+            } catch (error) {
+                hapi = {};
             }
         }
+    });
 
-        if (debug) {
-            console.log('Output Hapi:\r\n' + JSON.stringify(hapi));
-        }
-    }
+    /**
+     * Set up middleware including:
+     * - URI route handling
+     * - CORS request handling
+     * - CORS response handling
+     * - Hypermedia response handling
+     * - Unhandled Exception handling
+     *
+     * Order:
+     * 1. Request Middleware - Order Added.
+     * 2. Request Route
+     * 3. Response Route
+     * 4. Response Middleware - Order Reversed.
+     */
+    app
+        .use(router)
+        .use(function (handle) {
+            handle('request', function (env, next) {
+                /**
+                 * Cross-Origin Request
+                 * @see http://www.w3.org/TR/cors
+                 * @see http://www.html5rocks.com/static/images/cors_server_flowchart.png
+                 */
+                hapi.incomingMessageCors(env.request, env.cors = env.cors || {});
+                return next(env);
+            });
+        })
+        .use(function (handle) {
+            handle('response', function (env, next) {
+                /**
+                 * Cross-Origin Response
+                 * @see http://www.w3.org/TR/cors
+                 * @see http://www.html5rocks.com/static/images/cors_server_flowchart.png
+                 */
+                hapi.serverResponseCors(env.response, env.cors = env.cors || {});
+                return next(env);
+            });
+        })
+        .use(function (handle) {
+            handle('response', function (env, next) {
+                /**
+                 * Hypermedia Response
+                 */
+                hapi.serverResponseNavData(env.response, env.hypermedia);
+                return next(env);
+            });
+        })
+        .use(function (handle) {
+            handle('error', function (env, error, next) {
+                /**
+                 * Unhandled Exceptions
+                 */
+                console.error('The server had an unhandled exception. Exiting the process.');
+                console.error(new Date().toUTCString() + ' uncaughtException:', error.message);
+                console.error(error.stack);
+                env.response.statusCode = 500;
+                next(env);
+                process.exit(1);
+            });
+        });
+
+    /**
+     * Create a hypermedia.Api from the parsed api.json file.
+     * Map middleware and apis defined in the file to the HTTP Application Router.
+     */
+    hapi = hypermedia.Api.create(hapi);
+    hapi
+        .eachReqMiddleware(function (m) {
+            var handler = handlerById(dir, m.id);
+            app.use(function (handle) {
+                handle(m.middleware, handler || function (env, next) {
+                    if (debug) {
+                        console.log('The middleware, ' + m.id + ', needs a handler.');
+                    }
+                    return next(env);
+                });
+            });
+            if (debug) {
+                console.log('The middleware, ' + m.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
+            }
+        })
+        .eachApi(function (a) {
+            var handler = handlerById(dir, a.id);
+            app.route(a.uri, { 'methods': [ a.method ] }, function (handle) {
+                handle('request', function (env, next) {
+                    env.api = a;
+                    if (debug) {
+                        console.log('The api, ' + a.id + ', will be used.');
+                    }
+                    return next(env);
+                });
+                handle('request', handler || function (env, next) {
+                    env.response.statusCode = 200;
+                    if (debug) {
+                        console.log('The api, ' + a.id + ', needs a handler.');
+                    }
+                    return next(env);
+                });
+            });
+            if (debug) {
+                console.log('The api, ' + a.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
+            }
+        })
+        .eachResMiddleware(function (m) {
+            var handler = handlerById(dir, m.id);
+            app.use(function (handle) {
+                handle(m.middleware, handler || function (env, next) {
+                    if (debug) {
+                        console.log('The middleware, ' + m.id + ', needs a handler.');
+                    }
+                    return next(env);
+                });
+            });
+            if (debug) {
+                console.log('The middleware, ' + m.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
+            }
+        });
+
+    console.log('The server port is ' + hapi.port + '.');
+    console.log('The server hostname is ' + hapi.hostname + '.');
+
+    /**
+     * Create an HTTP Server with the Application Router.
+     */
+    http
+        .createServer(app.build().run)
+        .on('error', function (error) {
+            console.error('The server may need root user privileges to start.');
+            console.error(new Date().toUTCString() + ' caughtException:', error.message);
+            console.error(error.stack);
+        })
+        .on('listening', function () {
+            console.log('The server started.');
+        })
+        .listen(hapi.port, hapi.hostname);
 })();
