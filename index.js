9518cfe96e19d8fac6f65e31fef6fb95fd50772c
Tony Tahmouch <ttahmouch@gmail.com>
2014-10-28 15:23:14 -0400

    Command Line Interface Changes.
    The interface is now accessible from your PATH as a node.js global executable.
    The interface no longer accepts --debug and --dir. --debug has moved to a debug field in api.json. --dir is removed in favor of always using cwd.
    Handlers no longer use the Argo format. They use Express now as more people are familiar with that style.
    The interface now searches the cwd for a file called api.json and creates an Express server from the description.
    The interface also searches for files in cwd for api and middleware handlers of the naming convention [id].js.
    Slop was removed as a dependency because there is no longer a need for option flags, like --debug.
    Argo built-in middleware was converted to Express middleware, like CORS, hypermedia, etc.
    UglifyJS was added as a dependency because the hypermedia client is now built into a default resource on any coast server, /ua. The returned client is minified Javascript.
    HTTP was removed as a dependency because Express .listen() is used which creates an HTTP server internally.

--- index.js
+++ index.js
@@ -2,261 +2,199 @@
 
 /**
  * Falsy Values: false, 0, "", null, undefined, NaN
- *
- * - Command Line Usage:
- * node index.js --debug --dir=/path/to/api.json/and/handler/subdirectories
+ * Command Line Usage (Local):  node [path/to/]index.js
+ * Command Line Usage (Global): coast
  */
-(function () {
-    /**
-     * Module Dependencies.
-     * @type {exports}
-     */
-    var http = require('http'),
-        path = require('path'),
-        argo = require('argo'),
-        router = require('argo-url-router'),
-        hypermedia = require('hypermedia'),
-        slop = require('slop');
 
-    /**
-     * Walks a file system directory, and invokes the callback function at every encountered file.
-     *
-     * @param dir {string} representing a file system directory.
-     * @example "/"
-     *
-     * @param callback {Function} with a single 'file' path argument.
-     * @example
-     * function (file) {
-     *     var path = require('path'),
-     *         name = path.basename(file);
-     * }
-     *
-     * @return {undefined}
-     */
-    function walkSync(dir, callback) {
-        if (typeof dir === 'string' && typeof callback === 'function') {
-            try {
-                var fs = require('fs'),
-                    path = require('path'),
-                    stat = fs.lstatSync(dir);
-                if (stat.isFile()) {
-                    callback(dir);
-                } else if (stat.isDirectory()) {
-                    try {
-                        fs.readdirSync(dir).forEach(function (sub) {
-                            walkSync(path.join(dir, sub), callback);
-                        });
-                    } catch (error) {
-                        console.error('The path, ' + dir + ', cannot be walked.');
-                    }
+/**
+ * Walk a file system directory, and callback at every file.
+ *
+ * @param dir {String} representing a file system directory.
+ * @example "/"
+ * @param callback {Function} with a single 'file' path argument.
+ * @example function (file) { var name = require('path').basename(file); }
+ * @return {Undefined}
+ */
+function walkDirByFileSync(dir, callback) {
+    if (typeof dir === 'string' && typeof callback === 'function') {
+        try {
+            var fs = require('fs'),
+                path = require('path'),
+                stat = fs.lstatSync(dir);
+            if (stat.isFile()) {
+                callback(dir);
+            } else if (stat.isDirectory()) {
+                try {
+                    fs.readdirSync(dir).forEach(function (sub) {
+                        walkDirByFileSync(path.join(dir, sub), callback);
+                    });
+                } catch (error) {
+                    console.error(new Date().toUTCString() + ' caughtException:', error.message);
+                    console.error(error.stack);
                 }
-            } catch (error) {
-                console.error('The path, ' + dir + ', does not exist.');
             }
-        }
-    }
-
-    /**
-     * Walks a file system directory, and looks for Node Javascript files that export a handler function.
-     *
-     * @param dir {string} representing a file system directory.
-     * @example "/"
-     *
-     * @param id {string} representing the name of the file without the ".js" extension.
-     * @example "name"
-     *
-     * @return {Function} if a handler function was exported; {null}, otherwise.
-     */
-    function handlerById(dir, id) {
-        var handler = null;
-        if (typeof id === 'string' && typeof dir === 'string') {
-            walkSync(dir, function (file) {
-                var path = require('path'),
-                    name = path.basename(file, '.js');
-                if (name === id) {
-                    if (debug) {
-                        console.log('The ' + id + '.js directory is ' + file + '.');
-                    }
-                    try {
-                        handler = require(file);
-                    } catch (error) {
-                        handler = null;
-                    }
-                }
-            });
-        }
-        return typeof handler === 'function' ? handler : null;
-    }
-
-    /**
-     * Command Line Options.
-     * Dir will resolve a given path, or be the current working directory.
-     */
-    var opt = slop().parse(process.argv),
-        dir = path.resolve(typeof opt.get('dir') === 'string' ? opt.get('dir') : ''),
-        debug = opt.has('debug');
-
-    if (debug) {
-        console.log('The cwd directory is ' + process.cwd() + '.');
-        console.log('The api directory is ' + dir + '.');
-    }
-
-    /**
-     * HTTP Application Router.
-     */
-    var app = argo(),
-        hapi = {};
-
-    /**
-     * Walk the given directory in an attempt to find and parse a file called api.json.
-     */
-    walkSync(dir, function (file) {
-        var name = path.basename(file, '.json');
-        if (name === 'api') {
-            if (debug) {
-                console.log('The api.json directory is ' + file + '.');
-            }
-            try {
-                hapi = require(file);
-            } catch (error) {
-                hapi = {};
-            }
-        }
-    });
-
-    /**
-     * Set up middleware including:
-     * - URI route handling
-     * - CORS request handling
-     * - CORS response handling
-     * - Hypermedia response handling
-     * - Unhandled Exception handling
-     *
-     * Order:
-     * 1. Request Middleware - Order Added.
-     * 2. Request Route
-     * 3. Response Route
-     * 4. Response Middleware - Order Reversed.
-     */
-    app
-        .use(router)
-        .use(function (handle) {
-            handle('request', function (env, next) {
-                /**
-                 * Cross-Origin Request
-                 * @see http://www.w3.org/TR/cors
-                 * @see http://www.html5rocks.com/static/images/cors_server_flowchart.png
-                 */
-                hapi.incomingMessageCors(env.request, env.cors = env.cors || {});
-                return next(env);
-            });
-        })
-        .use(function (handle) {
-            handle('response', function (env, next) {
-                /**
-                 * Cross-Origin Response
-                 * @see http://www.w3.org/TR/cors
-                 * @see http://www.html5rocks.com/static/images/cors_server_flowchart.png
-                 */
-                hapi.serverResponseCors(env.response, env.cors = env.cors || {});
-                return next(env);
-            });
-        })
-        .use(function (handle) {
-            handle('response', function (env, next) {
-                /**
-                 * Hypermedia Response
-                 */
-                hapi.serverResponseNavData(env.response, env.hypermedia);
-                return next(env);
-            });
-        })
-        .use(function (handle) {
-            handle('error', function (env, error, next) {
-                /**
-                 * Unhandled Exceptions
-                 */
-                console.error('The server had an unhandled exception. Exiting the process.');
-                console.error(new Date().toUTCString() + ' uncaughtException:', error.message);
-                console.error(error.stack);
-                env.response.statusCode = 500;
-                next(env);
-                process.exit(1);
-            });
-        });
-
-    /**
-     * Create a hypermedia.Api from the parsed api.json file.
-     * Map middleware and apis defined in the file to the HTTP Application Router.
-     */
-    hapi = hypermedia.Api.create(hapi);
-    hapi
-        .eachReqMiddleware(function (m) {
-            var handler = handlerById(dir, m.id);
-            app.use(function (handle) {
-                handle(m.middleware, handler || function (env, next) {
-                    if (debug) {
-                        console.log('The middleware, ' + m.id + ', needs a handler.');
-                    }
-                    return next(env);
-                });
-            });
-            if (debug) {
-                console.log('The middleware, ' + m.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
-            }
-        })
-        .eachApi(function (a) {
-            var handler = handlerById(dir, a.id);
-            app.route(a.uri, { 'methods': [ a.method ] }, function (handle) {
-                handle('request', function (env, next) {
-                    env.api = a;
-                    if (debug) {
-                        console.log('The api, ' + a.id + ', will be used.');
-                    }
-                    return next(env);
-                });
-                handle('request', handler || function (env, next) {
-                    env.response.statusCode = 200;
-                    if (debug) {
-                        console.log('The api, ' + a.id + ', needs a handler.');
-                    }
-                    return next(env);
-                });
-            });
-            if (debug) {
-                console.log('The api, ' + a.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
-            }
-        })
-        .eachResMiddleware(function (m) {
-            var handler = handlerById(dir, m.id);
-            app.use(function (handle) {
-                handle(m.middleware, handler || function (env, next) {
-                    if (debug) {
-                        console.log('The middleware, ' + m.id + ', needs a handler.');
-                    }
-                    return next(env);
-                });
-            });
-            if (debug) {
-                console.log('The middleware, ' + m.id + ', ' + (handler ? 'has' : 'needs') + ' a handler.');
-            }
-        });
-
-    console.log('The server port is ' + hapi.port + '.');
-    console.log('The server hostname is ' + hapi.hostname + '.');
-
-    /**
-     * Create an HTTP Server with the Application Router.
-     */
-    http
-        .createServer(app.build().run)
-        .on('error', function (error) {
-            console.error('The server may need root user privileges to start.');
+        } catch (error) {
             console.error(new Date().toUTCString() + ' caughtException:', error.message);
             console.error(error.stack);
-        })
-        .on('listening', function () {
-            console.log('The server started.');
-        })
-        .listen(hapi.port, hapi.hostname);
-})();
+        }
+    }
+}
+
+/**
+ * Walk a file system directory, and require file by name.
+ *
+ * @param dir {String} representing a file system directory.
+ * @example "/"
+ * @param name {String} representing the name of the file.
+ * @example "name.js"
+ * @return {Object}
+ * @example module.exports
+ * @example null
+ */
+function requireFileByName(dir, name) {
+    var handler = null;
+    if (typeof dir === 'string' && typeof name === 'string') {
+        walkDirByFileSync(dir, function (file) {
+            var path = require('path');
+            if (path.basename(file) === name) {
+                try {
+                    handler = require(file);
+                } catch (error) {
+                    handler = null;
+                }
+            }
+        });
+    }
+    return handler;
+}
+
+/**
+ * Look for api.json in cwd.
+ */
+var lostFile = true;
+
+walkDirByFileSync(process.cwd(), function (file) {
+    var path = require('path');
+    if (path.basename(file) === 'api.json') {
+        /**
+         * Found api.json in cwd.
+         * Require Hypermedia as a module to use exported middleware in a server.
+         * Read and minify Hypermedia as a text file to use exported client in a browser.
+         */
+        var fs = require('fs'),
+            hypermedia = require('hypermedia'),
+            uglify = require('uglify-js'),
+            clientPath = require.resolve('hypermedia'),
+            readClient = fs.readFileSync(clientPath, 'utf-8'),
+            miniClient = uglify.minify(clientPath).code,
+            api = hypermedia.Api.createFromFile(file);
+
+        lostFile = false;
+
+        if (api.protocol === 'http') {
+            var express = require('express'),
+                app = express(),
+                lostMiddleware = [];
+
+            /**
+             * Default API route handler that acknowledges requests with a 200 OK.
+             *
+             * @param req {IncomingMessage} object created by http.Server.
+             * @see http://nodejs.org/api/http.html#http_http_incomingmessage
+             * @param res {ServerResponse} object created by http.Server.
+             * @see http://nodejs.org/api/http.html#http_class_http_serverresponse
+             * @param next {Function} middleware in the pipeline.
+             * @return {Undefined}
+             */
+            function defaultApi(req, res, next) {
+                res.statusCode = 200;
+                return next();
+            }
+
+            /**
+             * Default middleware handler that does nothing.
+             *
+             * @param req {IncomingMessage} object created by http.Server.
+             * @see http://nodejs.org/api/http.html#http_http_incomingmessage
+             * @param res {ServerResponse} object created by http.Server.
+             * @see http://nodejs.org/api/http.html#http_class_http_serverresponse
+             * @param next {Function} middleware in the pipeline.
+             * @return {Undefined}
+             */
+            function defaultMid(req, res, next) {
+                return next();
+            }
+
+            /**
+             * Create API with middleware/route stack:
+             * 1. Incoming Message CORS Middleware (Creates req.cors based on http.IncomingMessage.)
+             * 2. a. Client User Agent API (Returns API client from GET /ua.)
+             *    b. Custom api.json Middleware/APIs
+             * 3. Outgoing Message Body Middleware (Modifies res.body to String.)
+             * 4. Outgoing Message Hypermedia Middleware (Modifies res.body based on res.hype.)
+             * 5. Outgoing Message CORS Middleware (Modifies http.ServerResponse based on req.cors.)
+             * 6. Outgoing Message Final Middleware (Sends final res.body to client.)
+             */
+            app
+                .disable('x-powered-by')
+                .use(function (req, res, next) {
+                    api.incomingMessageCors(req, res);
+                    return next();
+                })
+                .get('/ua', function (req, res, next) {
+                    res.statusCode = 200;
+                    res.setHeader('content-type', 'application/javascript');
+                    res.body = api.debug ? readClient : miniClient;
+                    return next();
+                });
+
+            /**
+             * Look for [routine.id].js in cwd.
+             * Use custom middleware and APIs when found; default, otherwise.
+             */
+            api.each(function (routine, type) {
+                var handler = requireFileByName(process.cwd(), routine.id + '.js'),
+                    lostFile = typeof handler !== 'function';
+                if (lostFile) {
+                    lostMiddleware.push(routine.id);
+                }
+                if (type === 'mid') {
+                    app.use(lostFile ? defaultMid : handler);
+                }
+                if (type === 'api') {
+                    app[routine.method.toLowerCase()](routine.uri, lostFile ? defaultApi : handler);
+                }
+            });
+
+            if (lostMiddleware.length > 0) {
+                console.log('A handler file was not found for ' + lostMiddleware.join(', ') + '.');
+            }
+
+            app
+                .use(function (req, res, next) {
+                    api.outgoingMessageBody(req, res);
+                    return next();
+                })
+                .use(function (req, res, next) {
+                    api.outgoingMessageHypermedia(req, res);
+                    return next();
+                })
+                .use(function (req, res, next) {
+                    api.outgoingMessageCors(req, res);
+                    return next();
+                })
+                .use(function (req, res) {
+                    api.outgoingMessageFinal(req, res);
+                })
+                .listen(api.port, api.hostname, function () {
+                    console.log('A server is accepting connections on ' + api.hostname + ':' + api.port + '.');
+                });
+        } else {
+            console.log('A transfer protocol other than HTTP is not currently supported in api.json.');
+        }
+    }
+});
+
+if (lostFile) {
+    console.log('A file called api.json was not found within the current working directory.');
+}
